1bbb1f361807fd3d13c30d623c3792cde57934fd
cybrisha <cybrisha@gmail.com>
2016-08-22 07:08:32 +0300

    header completed

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,24 @@
 ;
-console.log('test');
+'use strict';
+
+(function () {
+
+  window.onload = toggleBtn;
+
+  console.log('WooHoo :D');
+
+  function toggleBtn() {
+    const collapseMenu = document.getElementById('collapse-menu'),
+          expandMenu = document.getElementById('expand-menu'),
+          headerView = document.getElementById('header');
+
+    collapseMenu.onclick = function (e) {
+      headerView.className = 'header collapsed';
+    };
+
+    expandMenu.onclick = function (e) {
+      headerView.className = 'header expanded';
+    };
+  }
+
+})();
